a9a8657e5bd4c29dbde7835c2c97500504b6efce
stefan Åhlander <cs.ahlander@gmail.com>
2020-06-18 10:01:45 +0200

    add handling of access headers

--- app.js
+++ app.js
@@ -27,6 +27,13 @@ const app = express();
 // Apply middleware
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next();
+});
+
 // Routes
 app.use(`${baseUri}/tasks`, tasksRoutes);
 app.use(`${baseUri}/info`, infoRoutes);
